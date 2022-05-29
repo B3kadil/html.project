af9469f846f077589342ed4e0e7d2d3599dc8031
Bekadil <bekadilseksenbaev03@gmail.com>
2022-05-29 17:41:55 +0600

    new project with udemy

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict.";
+// const obj = {
+//     name: "salam",
+//     lastname: "aleykum"
+// };
+// console.log(obj.name, obj.lastname);
+
+
+// const result = confirm("Are you here?");
+// console.log(result, typeof(result));
+// if (result == false) {
+//     console.log("its false")
+// } else {
+//    console.log("its true")
+
+// let result = +prompt("Salam");
+// console.log(Number.parseInt(result) + 3, typeof(result));
+
+// const answers = [];
+// answers[0] = prompt("1-10");
+// console.log(Number.parseInt(answers[0]) + 3);
+// console.log(Number.toString(answers[0]) + 3, typeof(answers[0]));
+
+
+const match = 87878962;
+console.log(`https://fastcup.com/math${match}`);
+const Name = prompt("Hello, what is your name?");
+alert(`Hello,${Name}!`)
