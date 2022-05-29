c558038d7350f0423d9876044d0b725a617ad992
Bekadil <bekadilseksenbaev03@gmail.com>
2022-05-29 18:10:31 +0600

    1 task of udemy

--- script.js
+++ script.js
@@ -22,7 +22,48 @@
 // console.log(Number.toString(answers[0]) + 3, typeof(answers[0]));
 
 
-const match = 87878962;
-console.log(`https://fastcup.com/math${match}`);
-const Name = prompt("Hello, what is your name?");
-alert(`Hello,${Name}!`)
+// const match = 87878962;
+// console.log(`https://fastcup.com/math${match}`);
+// const Name = prompt("Hello, what is your name?");
+// alert(`Hello,${Name}!`)
+
+
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let personalMoviDB = {
+    count: Number.parseInt(numberOfFilms),
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+personalMoviDB.movies = {
+    name: prompt("Один из последних просмотренных фильмов?"),
+    raiting: prompt("На сколько оцените его?")
+};
+console.log(personalMoviDB.count);
+console.log(personalMoviDB.movies.name, Number.parseInt(personalMoviDB.movies.raiting));
+let rait = Number.parseInt(personalMoviDB.movies.raiting);
+console.log(typeof(rait));
